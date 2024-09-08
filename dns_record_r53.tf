1e18da810925402f477a8eba5c96e6cbbdd8198c
alex <espoirerukundo405@gmail.comgit loggit config --global user.email espoirerukundo405@gmail.com>
2024-09-07 18:15:06 -0700

    lampstack

--- dns_record_r53.tf
+++ dns_record_r53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "rc1" {
-  zone_id = "Z0319926K2HLBRS8NWD0"
+  zone_id = "Z03Z075556619SK86Q4CT4H5"
   type    = "A"
   ttl     = 300
-  name    = "resume.kayband.com"
+  name    = "resume.espoirerukundo.homes"
   records = [aws_lightsail_instance.server1.public_ip_address]
 }
---
+++ valiables.tf
@@ -0,0 +1,12 @@
+variable "lamp_server" {
+    default = "lamp-server"
+ 
+}
+variable "bundle_id" {
+    default = "small_1_0"
+  
+}
+variable "availability_zone" {
+    default = "us-eat-1a"
+  
+}
